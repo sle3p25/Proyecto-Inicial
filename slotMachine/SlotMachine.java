c49e0426a8946a09a7ca401f0ecb0e34d3a98e02
julianRodriguez <julian25r2007@gmail.com>
2026-08-22 12:50:59 -0500

    Mini-ciclo 1: crear maquina y gestionar ruedas

---
+++ slotMachine/SlotMachine.java
@@ -0,0 +1,68 @@
+
+/**
+ * A slot machine simulator compound of several wheels
+ *
+ * @author Juan Pulido - Julian Rodriguez
+ * @version August 22
+ */
+
+import java.util.ArrayList;
+
+public class SlotMachine
+{
+    private ArrayList<Wheel> wheels;
+    private boolean ok;
+
+    /**
+     * Constructor for objects of class SlotMachine
+     */
+    public SlotMachine()
+    {
+        wheels = new ArrayList<Wheel>();
+        ok = true;
+    }
+
+    /**
+     * Adds an empty wheel on the indicated position.
+     */
+    public void addWheel(int pos) 
+    {
+        int index = clamp(pos - 1, 0, wheels.size());
+        wheels.add(index, new Wheel());
+        succeed();
+    }
+    
+    /**
+     * Delete a wheel on the indicated position.
+     */
+    public void delWheel(int pos) {
+        if (wheels.isEmpty()){
+            fail();
+            return;
+        }
+        int index = clamp(pos - 1, 0, wheels.size() -1);
+        wheels.remove(index);
+        succeed();
+    }
+    
+    /**
+     * @return true if the last operation was succeed
+     */
+    public boolean ok(){
+        return ok;
+    }
+    
+    private void succeed(){
+        ok = true;
+    }
+    
+    private void fail(){
+        ok = false;
+    }
+    
+    private int clamp(int value, int min, int max) {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+}
---
+++ slotMachine/Wheel.java
@@ -0,0 +1,34 @@
+
+/**
+ * Wheel.
+ * Represents a slot machine's wheel.
+ *
+ * @author Juan Pulido - Julian Rodriguez
+ * @version August 22
+ */
+public class Wheel
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Wheel
+     */
+    public Wheel()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
